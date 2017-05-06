fd7e60c1917407b07046eee32c18ef0d617a80c1
Race Soul <tjm-photo@mail.ru>
2017-05-06 12:06:08 +0300

    Добавил seed-ы для User, Roles

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,16 +1,23 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
-class UsersTableSeeder extends Seeder
-{
+
+class UsersTableSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+        DB::table('Users')->delete();
+        User::create([
+            'name' => 'First name',
+            'email' => 'hagrid@yandex.ru',
+            'password' => '111111',
+        ]);
     }
+
 }
